03e082ad04f0131f838527bfea8342dc3eb29a4f
Yaroslav Gusev <yarigusev@gmail.com>
2024-11-12 17:25:07 +0300

    delete all -> Hello World

--- task_lesson_2.py
+++ task_lesson_2.py
@@ -1,6 +1,8 @@
-from selene import browser, be, have
+# from selene import browser, be, have
+#
+#
+# browser.open('https://duckduckgo.com/')
+# browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
+# browser.element('[id=react-layout]').should(have.text('User-oriented Web UI browser tests in Python'))
 
-
-browser.open('https://duckduckgo.com/')
-browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id=react-layout]').should(have.text('User-oriented Web UI browser tests in Python'))
+print('Hello World')
